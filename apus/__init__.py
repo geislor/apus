2394deb0919d0c892ed165e4f866aa962619d760
Geislor Crestani <geislor.crestani@siteblindado.com.br>
2016-06-25 10:38:20 -0300

    0.1.6

--- apus/__init__.py
+++ apus/__init__.py
@@ -2,4 +2,14 @@
 
 __author__ = 'Geislor Crestani'
 __email__ = 'geislor@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
+
+import os
+import sys
+
+abspath = lambda *p: os.path.abspath(os.path.join(*p))
+CONFIG_DIR = abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(CONFIG_DIR)
+PROJECT_ROOT = os.path.dirname(CONFIG_DIR)
+sys.path.insert(0, os.path.join(PROJECT_DIR, "apus"))
+
