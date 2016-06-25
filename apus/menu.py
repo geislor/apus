ff04ef56be2a2b1057b987d4880963598bd7f49b
Geislor Crestani <geislor.crestani@siteblindado.com.br>
2016-06-24 22:12:13 -0300

    teste2

--- apus/menu.py
+++ apus/menu.py
@@ -1,27 +1,20 @@
 # -*- coding:utf-8 -*-
-
-import click
 from .users.models import Users, session
 
 
-@click.command()
-@click.option('--user', help='Iniciar configurações: (createuser: Adiciona '
-                             'novo usuário. ')
-# @click.option('--count', default=1, help='number of greetings.')
-# @click.option('--name', prompt='Your name', help='The person to greet.')
-def general(user):
+def general(command):
     """
     Hello!
     """
 
-    if user == 'create':
+    if command == 'create':
         print("Criar novo usuário")
         username = input('username: ')
         email = input('email: ')
         user = Users(username=username, email=email)
         user.save()
 
-    elif user == 'listall':
+    elif command == 'listall':
         print("Listagem de usuários: ")
         usu = session.query(Users)
         for i, u in enumerate(usu.all()):
